5322d702b896cfc31435ddee6807db6bb4474f37
Bonkook Koo <justinaus2k@gmail.com>
2022-06-29 23:47:32 +0900

    Move src 폴더 밑으로

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css';
+import '../src/styles/globals.css';
 
 import { CacheProvider, EmotionCache } from '@emotion/react';
 import { CssBaseline, ThemeProvider } from '@mui/material';
