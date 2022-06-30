1f039163ed384f7ee0f414affb153a27272d50f5
bonkook koo <justinaus2k@gmail.com>
2022-06-30 21:12:09 +0900

    Add firebase

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+  },
 };
 
 module.exports = nextConfig;
