09b1b4bd7f4248b9123211e2abe05805f329f31a
Bonkook Koo <justinaus2k@gmail.com>
2022-07-05 01:07:24 +0900

    Add mui icons, svgr

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@ const nextConfig = {
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
     NAVER_CLIENT_ID: process.env.NAVER_CLIENT_ID,
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
